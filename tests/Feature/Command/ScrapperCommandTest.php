e5943ff915425f790e659ff9911bbf641eb81ad2
Miguel Ángel Sánchez Palafox <miguel.sanchez@datoz.com>
2019-09-12 21:43:46 -0500

    Add goutter to scrapper and save data from linio on database

---
+++ tests/Feature/Command/ScrapperCommandTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Tests\Feature\Command;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Symfony\Component\DomCrawler\Crawler;
+use Tests\TestCase;
+use Weidner\Goutte\GoutteFacade;
+
+/**
+ * Class ScrapperCommand
+ * @package Tests\Feature\Command
+ */
+class ScrapperCommandTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test  */
+    public function save_crawled_data()
+    {
+        $crawler = new Crawler(file_get_contents(__DIR__ . '/Linio/Success.html'));
+        GoutteFacade::shouldReceive('request')
+            ->once()
+            ->andReturn($crawler)
+        ;
+
+        $this->artisan('make:scrapper')
+        ->expectsOutput('something')
+        ->assertExitCode(0)
+        ;
+
+        $this->assertEquals(10, Product::count());
+    }
+
+    /** @test */
+    public function throw_exception_when_has_minus_of_10_item()
+    {
+        $crawler = new Crawler(file_get_contents(__DIR__ . '/Linio/Minus10.html'));
+        GoutteFacade::shouldReceive('request')
+            ->once()
+            ->andReturn($crawler)
+        ;
+
+        $this->artisan('make:scrapper')
+            ->expectsOutput('do not exist enough Item or change linio structured html')
+            ->assertExitCode(1)
+        ;
+
+    }
+}
